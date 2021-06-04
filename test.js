bc86846e9765bc9630cf808115a37ed3b75e6184
James Bonanno <james@atlantixeng.com>
2021-06-04 15:01:26 -0400

    deleted typescript files (unnecessary)

--- test.js
+++
@@ -1,7 +0,0 @@
-var score1 = 50;
-var score2 = 42.50;
-var sum = score1 + score2;
-console.log("namex:");
-console.log("first score: " + score1);
-console.log("second score: " + score2);
-console.log("sum of the scores: " + sum);
